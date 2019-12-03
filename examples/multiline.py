56854667d00477abd2d414ffe5be62a6f5306750
Johnny Gill <swfiua@gmail.com>
2019-12-02 19:00:48 -0500

    multiline example

---
+++ examples/multiline.py
@@ -0,0 +1,23 @@
+"""
+Tools to help testing with image comparison.
+"""
+import sys
+from matplotlib import pyplot as plt
+from blume.table import table
+
+cell_text = [["long text\nWith\nmany\nlines"],
+                 ['more but short']]
+                 
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1, 1)
+ax.axis('off')
+
+# Add a table at the top of the axes
+table(
+    ax,
+    cellText=cell_text,
+    bbox=(0, 0, 1, 1))
+
+if not sys.flags.interactive:
+    plt.show()
