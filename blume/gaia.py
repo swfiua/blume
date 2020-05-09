77a2efe7b28fc8a8a3419992470f20a302af1019
Johnny Gill <swfiua@gmail.com>
2020-05-09 18:55:01 -0400

    re organise gaia module now works with farm

--- blume/gaia.py
+++ blume/gaia.py
@@ -9,6 +9,7 @@ This module uses the `astroquery.gaia` submodule to query the Gaia database.
 
 
 """
+from astropy.table import Table
 from astroquery.gaia import Gaia
 from .magic import Ball
 
@@ -19,86 +20,118 @@ import healpy as hp
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-import pathlib
+from pathlib import Path
+
+from . import magic
+
+from . import farm as fm
 
 TABLE = 'gaiadr2.gaia_source'
 
 COLUMNS = ('source_id', 'random_index', 'ra', 'dec', 'parallax', 'radial_velocity')
 
 
+
+def get_sample(self):
+
+    columns = (', ').join(COLUMNS)
+    table = TABLE
+    sample = tuple(range(1,100001))
+    squeal = f'select {columns} from {table} where random_index in {sample}'
+    #squeal = f'select top 1000 {columns} from {table} where mod(random_index, 1000000) = 0'
+
+
+    #print(f'squeal: {squeal}')
+        
+    job = Gaia.launch_job_async(squeal)
+
+    return job.get_results()
+
+
+
+
 class Milky(Ball):
 
-    def __init__(self):
+    def __init__(self, table):
 
         super().__init__()
 
-    def get_sample(self):
-
-        columns = (', ').join(COLUMNS)
-        table = TABLE
-        sample = tuple(range(1,1001))
-        squeal = f'select {columns} from {table} where random_index in {sample}'
-        #squeal = f'select top 1000 {columns} from {table} where mod(random_index, 1000000) = 0'
-
-
-        #print(f'squeal: {squeal}')
+        self.table = table
+        self.level = 1
         
-        job = Gaia.launch_job_async(squeal)
-
-        #print(job.get_results())
-
-        return job
-
-    async def get_results(self, job):
-
-        while not job.is_finished():
-            print('checking for job finish')
-            await curio.sleep(10)
-
-        return job.get_results()
+        self.add_filter('z', self.zoom)
+        self.add_filter('x', self.xzoom)
 
     async def run(self):
 
 
-        job = self.get_sample()
+        level = self.level
+        table = self.table
+        
+        nside = 2 ** level
 
-        table = await self.get_results(job)
+        indices = hp.ang2pix(
+            nside,
+            [x['ra'] for x in table],
+            [x['dec'] for x in table],
+            lonlat=True)
+    
+        npix = hp.nside2npix(nside)
+        print('npix, nside, level', npix, nside, level)
 
-        return table
 
+        hpxmap = np.zeros(npix, dtype=np.float)
+
+        for row, index in zip([item for item in table], indices):
+
+            ix = row['source_id'] >> 35
+            #ix = row['source_id'] >> 35
+
+            #assert(index == ix)
+            hpxmap[ix // 4**(12 - level)] += 1
+            #hpxmap[index] += 1
+
+        #print(hpxmap)
+        hp.mollview(hpxmap, coord=('E', 'G'), nest=True)
+
+        await self.put(magic.fig2data(plt))
+
+    async def zoom(self):
+
+        if self.level < 12:
+            self.level += 1
+
+    async def xzoom(self):
+
+        if self.level > 0:
+            self.level -= 1
+
+async def run(table):
+
+    milky = Milky(table)
+
+    farm = fm.Farm()
+    farm.add(milky)
+
+    await farm.start()
+
+    await farm.run()
 
 
 if __name__ == '__main__':
 
-    milky = Milky()
+    path = Path('tab.fits')
+    if path.exists():
+        table = Table.read(path, 'fits')
 
-    table = curio.run(milky.run())
+    else:
+        table = get_sample()
 
-    level = 12
-    nside = 2 ** level
+        # be nice, cache data
+        table.write(path, 'fits')
 
-    indices = hp.ang2pix(
-        nside,
-        [x['ra'] for x in table],
-        [x['dec'] for x in table],
-        lonlat=True)
+    curio.run(run(table))
+    
 
-    npix = hp.nside2npix(nside)
-    print('npix, nside, level', npix, nside, level)
-
-
-    hpxmap = np.zeros(npix, dtype=np.float)
-
-    for row, index in zip([item for item in table], indices):
-        print('xx', index)
-
-        ix = row['source_id'] >> 35 + (2 * (12-level))
-        print(index, ix)
-        #assert(index == ix)
-        #hpxmap[ix] += 1
-        hpxmap[index] += 1
-
-    print(hpxmap)
-    hp.mollview(hpxmap)
-
-    plt.show()
+    
+    
