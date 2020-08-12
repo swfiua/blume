474e4ae7afd0f86924bc3bf5b41405a38018a4e9
Johnny Gill <swfiua@gmail.com>
2020-08-11 22:00:31 -0400

    how to work with sympy and einsteinpy too

--- blume/dss.py
+++ blume/dss.py
@@ -3,13 +3,39 @@
 Are gamma-ray bursts optical illusions?
 
 Robert S Mackay, Colin Rourke.
+
+See notebook for evolving ideas.
+
+Einsteinpy has all sorts of goodies that are helpful here.  I am just starting
+to find my way around the project.
+
+The symbolic part has a predefined deSitter metric among others.
+
+It was there I discovered there is a Gödel metric, an exact solution of the
+Einstein field equations.
+
+The metric is based on a spinning universe.  Gödel was known to keep asking,
+"has the universe started spinning yet?".   
+
+A rotating universe was central to his solution and had some peculiar
+properties such as observers being able to see themselves at some time in the
+past.  Although, that's what we see every time we look in a mirror.
+
+If our universe is vast beyond the visible and the cosmic microwave background
+is a glimmer of what is beyond, then I think we can see the universe spinning.
+
 """
 
 # we are going to need this
 import math
+import numpy as np
+from matplotlib import pyplot as plt
 
 # hmm, sympy is a whole universe of stuff
 from sympy import *
+from einsteinpy.symbolic import predefined
+
+import curio
 
 from blume import magic
 from blume import magic
@@ -19,6 +45,8 @@ class Dss(magic.Ball):
 
     def __init__(self):
         """ initialise """
+        super().__init__()
+        
         self.alpha, self.beta, self.gamma, self.delta = symbols(
             'alpha beta gamma delta')
 
@@ -32,32 +60,74 @@ class Dss(magic.Ball):
 
     def constraints(self):
 
-        assert(self.alpha**2 - self.gamma**2 >= 1.)
+        print(self.alpha**2 - self.gamma**2 >= 1.)
 
-        assert(self.alpha > 0)
+        print(self.alpha > 0)
 
         a, b, c, d = self.alpha, self.beta, self.gamma, self.delta
 
-        assert((a * b - c * d) <= (a*a - c*c - 1) * (b*b - d*d -1))
+        print((a * b - c * d) <= (a*a - c*c - 1) * (b*b - d*d -1))
 
-        assert((a*d - b*c) <= (a*a - c*c - b*b + d*d -1))
+        print((a*d - b*c) <= (a*a - c*c - b*b + d*d -1))
 
-    def blue_shift_time(self, alpha, delta):
+    def blue_shift_time(self, alpha=None, delta=None):
         """ """
-        sqrt = math.sqrt
+        a = sqrt(alpha or self.alpha)
+        d = sqrt(delta or self.delta)
 
-        a = sqrt(alpha)
-        d = sqrt(delta)
-
-        etb = sqrt((1+a+d)/d) + sqrt((1+a-d)/d)
+        etb = sqrt((1+a+d)/(2*d)) + sqrt((1+a-d)/d)
         
-        return math.log(math.sqrt(etb))
-        
+        return log(etb)
+
+    def deSitter(self):
+
+        return predefined.DeSitter()
 
 
-if __name__ == '__main__':
+    async def run(self):
+
+        size = 100
+        epsilon = 1e-3
+
+        img = np.zeros((size, size))
+
+        for row in range(1, size+1):
+
+            delta = row/(2*(size+1))
+            for col in range(1, size+1):
+                alpha = col/(size+1)
+            
+                img[row-1][col-1] = self.blue_shift_time(
+                    alpha or epsilon, delta or epsilon)
+ 
+            print(img[row-1])
+
+        plt.imshow(img, cmap=magic.random_colour())
+        await self.put(magic.fig2data(plt))
+
+async def run():
 
     dss = Dss()
 
     dss.set_abcd()
     dss.constraints()
+
+    farm = fm.Farm()
+    
+    farm.add(dss)
+    
+    await farm.start()
+
+    print(dss.deSitter())
+
+    farm.shep.path.append(dss)
+
+    await farm.run()
+        
+
+
+if __name__ == '__main__':
+
+    init_printing(pretty_print=True)
+
+    curio.run(run(), with_monitor=True)
