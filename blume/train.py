09979f655ca9d7f629a2277f45e4d3039b731691
Johnny Gill <swfiua@gmail.com>
2023-08-21 13:53:03 -0400

    train hacks for jwst fits files

--- blume/train.py
+++ blume/train.py
@@ -14,7 +14,7 @@ import random
 
 import numpy as np
 
-from collections import deque
+from collections import deque, defaultdict, Counter
 import time
 import argparse
 
@@ -24,20 +24,13 @@ from blume import farm
 
 class Train(magic.Ball):
 
-    def __init__(self, path='.', paths=None):
+    def __init__(self, args=None):
 
         super().__init__()
-        
-        self.path = Path(path)
-        self.paths=paths
-        self.scale = 0
-        self.size = 1024
-        self.rotation = -1
-        self.clip = None
-        self.cmap = None
-        self.min_entropy = .0
 
-        self.boost = 0
+        parser = self.get_parser()
+
+        self.update(parser.parse_args(args))
 
         def reverse():
             """ U turn if U want 2 """
@@ -46,6 +39,23 @@ class Train(magic.Ball):
         self.add_filter('d', self.down)
         self.add_filter('b', self.back)
 
+    def get_parser(self):
+        """ Use argparse to get arguments from command line """
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--path', default='.')
+        parser.add_argument('--paths', default=[], nargs='+')
+        parser.add_argument('--clip', type=float, default=0)
+        parser.add_argument('--scale', type=float, default=0)
+        parser.add_argument('--size', type=int, default=1024)
+        parser.add_argument('--rotation', type=int, default=-1)
+        parser.add_argument('--cmap', default=None)
+        parser.add_argument('--min_entropy', type=float, default=.0)
+        parser.add_argument('--boost', type=float, default=0)
+        parser.add_argument('--rgb', action='store_true', default=False)
+
+        return parser
+        
 
     async def down(self):
 
@@ -67,6 +77,9 @@ class Train(magic.Ball):
 
     async def start(self):
 
+        # turn path into a Path
+        self.path = Path(self.path)
+        
         if not self.paths:
             if self.path.is_file():
                 self.paths = [self.path]
@@ -77,6 +90,7 @@ class Train(magic.Ball):
         else:
             self.paths = [Path(path) for path in self.paths]
         self.paths = deque(sorted(self.paths))
+        self.scan()
         
         print('PATHS', len(self.paths))
         self.bans = ['embedding', '_runner', 'tick_labels']
@@ -84,6 +98,59 @@ class Train(magic.Ball):
         # not sure this works -- stop others stealing the show
         self.bads = set()
 
+    def scan(self):
+        """ Scan current position
+
+        Idea is a file browser that shows tables so
+        you can select what to view.
+        
+        Build a table.
+        """
+        paths = self.paths
+        suffix = set(x.suffix for x in paths)
+        from astropy import table
+        tab = []
+        colnames = []
+        for path in paths:
+            parts = path.parts
+            fields = path.stem.replace('-', ' ').replace('_', ' ').split()
+            row = list(parts[1:]) + list(fields) + [path.suffix]
+
+            tab.append(row)
+
+        tab = table.Table(tab)
+        
+        self.put_nowait(tab, 'help')
+        self.put_nowait(self.table_count(tab), 'help')
+
+    def table_count(self, table, maxrows=None):
+        """ Do some counts on a table 
+        
+        pretty sure there is some sort of table.info.stats()
+        """
+        from blume import taybell
+        msg = []
+        counters = defaultdict(Counter)
+        for ix, row in enumerate(table):
+            if maxrows and ix > maxrows:
+                break
+            
+            for key in row.colnames:
+                value = row[key]
+                if isinstance(value, np.ma.core.MaskedConstant):
+                    value = value.tolist()
+                counters[key].update([value])
+
+        topn = 1
+        for key in table.colnames:
+            for value, count in counters[key].most_common(topn):
+                value = taybell.shortify_line(str(value), 20)
+                
+                msg.append([key, value, count])
+
+        return msg
+            
+        
     async def run(self):
 
         idx = 0
@@ -105,22 +172,21 @@ class Train(magic.Ball):
 
         try:
             image = Image.open(path)
+            w, h = image.size
+
+            scale = self.scale
+
+            if scale == 0:
+                scale = min(self.size/w, self.size/h)
+
+            if scale:
+                image = image.resize((int(w * scale), int(h * scale)))
+
         except:
-            print('BAD ONE', path)
-            self.bads.add(str(path))
-            return
-
-
-        w, h = image.size
-
-        scale = self.scale
-
-        if scale == 0:
-            scale = min(self.size/w, self.size/h)
-
-        if scale:
-            image = image.resize((int(w * scale), int(h * scale)))
-
+            # maybe its fits
+            if path.suffix == '.fits':
+                image = await self.fits_open(path)
+                
         if self.clip:
 
             image = np.clip(image, 0, self.clip)
@@ -149,6 +215,24 @@ class Train(magic.Ball):
         
         ax.show()
 
+    async def fits_open(self, path):
+
+        from astropy.io import fits
+        
+        tab = fits.open(path)
+        print(tab.info())
+        for item in tab:
+            print(item.size)
+        if isinstance(item.size, int):
+            print('Array size:', item.size)
+        elif item.size:
+            #await self.show_stats(item)
+            pass
+
+        print(tab[1].header)
+        self.tab = tab
+        return tab[1].data
+
 
     def booster(self, im):
         """ Scale pixel values in im by self. boost """
@@ -166,22 +250,16 @@ class Train(magic.Ball):
 
         return im
 
-async def run(args):
+async def run(args=None):
 
     fm = farm.Farm()
 
-    examples = Train(args.path, paths=args.paths)
+    train = Train(args)
 
-    fm.add(examples)
-
+    fm.add(train)
     await farm.start_and_run(fm)
-    
+
         
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', default='.')
-    parser.add_argument('--paths', default=[], nargs='+')
-
-    args = parser.parse_args()
-    magic.run(run(args))
+    magic.run(run())
